60e6e1a66d97e0cb5f8d9e291360a2fca1f4843a
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2024-02-18 23:59:20 +0530

    created about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
+      <Navbar />
+      <AboutMe />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import useData from "../hooks/useData";
 
 const AboutMe = () => {
+  const { data, loading } = useData();
+  if (loading) {
+    return <div>loading....</div>;
+  }
   return (
-    <div className="flex">
-      <div>
-        <div className="text-gray-500">I'm</div>
+    <div className="flex justify-between">
+      <div className="m-5 w-[50rem]">
+        <div className="text-gray-500 text-3xl">I'm</div>
+        <div className="text-6xl my-5">{data.name}</div>
+        <div className="text-lg text-gray-500">{data.bio}</div>
       </div>
       <div>
-        <img src="" />
+        <a href={data.html_url}>
+          <img
+            className="h-80 border-sky-500 m-5 border-8 rounded-full"
+            src={data.avatar_url}
+          />
+        </a>
       </div>
     </div>
   );
